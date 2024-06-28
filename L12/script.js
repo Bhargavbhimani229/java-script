6c831edfea66b4e731d2fb342af4c90cb491db11
Bhargavbhimani229 <bhargavbhimani229@gmail.com>
2024-06-28 11:45:44 +0530

    baisc program

---
+++ L12/script.js
@@ -0,0 +1,32 @@
+// let r=5,k;
+// for(let i=1;i<=r;i++){
+//   let line = " ";
+//   for(let j=1;j<=r;j++){
+//    if(j<=6-i){//(j<=i)
+//       line +=" *"; 
+//     }
+//     else{
+//       line +=" ";
+//     }
+//   }
+//   console.log(line);
+// }
+
+// 1. factorial number
+// let n=6;
+// function factorial(n) {
+//   if (n === 0 || n === 1) {
+//     return 1;
+//   } else {
+//     return n * factorial(n - 1);
+//   }
+// }
+
+// console.log(factorial(n)); 
+
+// let n=5,factorial=1;
+// while(n>1){
+//  factorial*=n; //factorial = factorial * n
+//  n--;
+// }
+// console.log(factorial);
---
+++ L14/script.js
@@ -0,0 +1,16 @@
+const units=prompt("Enter units");
+let bill=0;
+
+if(units<=50){
+  bill=units*0.50;
+}else if(units<=150){
+  bill=25+(units-50)*0.75;
+}else if(units<=250){
+  bill=100+(units-150)*1.20;
+}else{
+  bill=220+(units-250)*1.50;
+}
+
+const surcharge=bill*0.20
+const totalbill=bill+surcharge;
+console.log("totalbill"+totalbill);
